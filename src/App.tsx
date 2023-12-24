342c256284840128de3ee137a906852b61c5bb8a
jun <jun@Ubt2200.aaaaa>
2023-12-24 18:02:05 +0900

    color設定もで来た

--- src/App.tsx
+++ src/App.tsx
@@ -48,9 +48,16 @@ const Box = (boxprops: BoxProps) => {
     },
     color: {
       value: 'lime',
-      // onChange: (v) => {
-      //   // ref.current.material.color = new THREE.Color(v)
-      // },
+      onChange: (v: string) => {
+        if(boxprops.props.name == "meshBasicMaterial")
+          (ref.current.material as THREE.MeshBasicMaterial)!.color = new THREE.Color(v)
+        else if(boxprops.props.name == "meshNormalMaterial")
+          {/*(ref.current.material as THREE.MeshNormalMaterial)!.color = new THREE.Color(v)*/}
+        else if(boxprops.props.name == "meshPhongMaterial")
+          (ref.current.material as THREE.MeshPhongMaterial)!.color = new THREE.Color(v)
+        else if(boxprops.props.name == "meshStandardMaterial")
+          (ref.current.material as THREE.MeshStandardMaterial)!.color = new THREE.Color(v)
+      },
     },
   })
 
@@ -67,10 +74,10 @@ const App = () => {
       <Canvas camera={{ position: [3, 1, 2] }}>
         <ambientLight />
         <directionalLight />
-        <Box props={{position:[-0.75, 0, 0], name:"meshBasicMaterial",    material: new THREE.MeshBasicMaterial()}}/>
-        <Box props={{position:[ 0.75, 0, 0], name:"meshNormalMaterial",   material: new THREE.MeshNormalMaterial()}}/>
-        <Box props={{position:[-0.75, 2, 0], name:"meshPhongMaterial",    material: new THREE.MeshPhongMaterial()}}/>
-        <Box props={{position:[ 0.75, 2, 0], name:"meshStandardMaterial", material: new THREE.MeshStandardMaterial()}}/>
+        <Box props={{position:[-1, 0, 0], name:"meshBasicMaterial",    material: new THREE.MeshBasicMaterial()}}/>
+        <Box props={{position:[ 1, 0, 0], name:"meshNormalMaterial",   material: new THREE.MeshNormalMaterial()}}/>
+        <Box props={{position:[-1, 2, 0], name:"meshPhongMaterial",    material: new THREE.MeshPhongMaterial()}}/>
+        <Box props={{position:[ 1, 2, 0], name:"meshStandardMaterial", material: new THREE.MeshStandardMaterial()}}/>
         <OrbitControls />
         <axesHelper args={[5]} />
         <gridHelper />
